993f5f57d10f4edf4c3b15bced638c3925254ef9
Mohd Naushad <113180514+Naushad-ansarintiw@users.noreply.github.com>
2026-06-19 11:48:45 +0530

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.cpp
@@ -0,0 +1,32 @@
+
+
+class Solution {
+public:
+    set<vector<int>> st; 
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans; 
+        st.clear(); 
+        vector<int> vec; 
+        func(0, nums, vec, nums.size()); 
+        for(auto it: st) {
+            ans.push_back(it); 
+        }
+        return ans;
+    }
+
+    void func(int idx, vector<int>& nums, vector<int>& vec, int n) {
+        if(idx == n) {
+            st.insert(vec);
+            return; 
+        }
+
+        // take 
+        vec.push_back(nums[idx]);
+        func(idx+1, nums, vec, n); 
+
+        // not take 
+        vec.pop_back(); 
+        func(idx+1, nums, vec, n); 
+    }
+};
